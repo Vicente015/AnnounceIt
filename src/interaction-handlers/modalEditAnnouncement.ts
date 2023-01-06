dd038c455478f8d218af5d0d5c94e424327faa4e
Vicente <33096355+Vicente015@users.noreply.github.com>
2023-01-06 20:29:59 +0000

    ✨ edit command
    needs testing ;)

--- src/interaction-handlers/modalEditAnnouncement.ts
+++ src/interaction-handlers/modalEditAnnouncement.ts
@@ -1,23 +1,17 @@
 import { EmbedLimits, TextInputLimits } from '@sapphire/discord-utilities'
 import { InteractionHandler, InteractionHandlerTypes, PieceContext } from '@sapphire/framework'
-import { colord, extend } from 'colord'
-import namesPlugin from 'colord/plugins/names'
 import type { ModalSubmitInteraction } from 'discord.js'
 import ow from 'ow'
 import { Announcement } from '../schemas/Announcement'
 import isValidColorFormat from '../utils/colorValidation'
-import { temporaryImgStorage } from '../utils/Globals'
 import { reply } from '../utils/reply'
 import { validaModalInput } from '../utils/validateOptions'
 
-extend([namesPlugin])
-
 const Schema = ow.object.exactShape({
-  // name: ow.string.minLength(3).maxLength(16),
   // eslint-disable-next-line sort/object-properties
   description: ow.string.nonEmpty.maxLength(TextInputLimits.MaximumValueCharacters).message(() => 'commands:add-translation.descriptionMaxLength'),
-  title: ow.optional.string.nonEmpty.maxLength(EmbedLimits.MaximumTitleLength).message(() => 'commands:add-translation.titleMaxLength'),
   footer: ow.optional.string.nonEmpty.maxLength(EmbedLimits.MaximumFooterLength).message(() => 'commands:add-translation.footerMaxLength'),
+  title: ow.optional.string.nonEmpty.maxLength(EmbedLimits.MaximumTitleLength).message(() => 'commands:add-translation.titleMaxLength'),
   url: ow.optional.string.nonEmpty.url.message(() => 'commands:add-translation.urlNotValid'),
   color: ow.optional.string.nonEmpty.validate((string) => ({
     message: () => 'commands:add.notValidColor',
@@ -34,38 +28,48 @@ export class ModalHandler extends InteractionHandler {
   }
 
   public override parse (interaction: ModalSubmitInteraction) {
-    if (!interaction.customId.startsWith('addAnnouncement:')) return this.none()
+    if (!interaction.customId.startsWith('editAnnouncement:')) return this.none()
     return this.some()
   }
 
   public async run (interaction: ModalSubmitInteraction) {
     const options = await validaModalInput(interaction, Schema)
     if (!options) return
-    let { color, description, footer, t, title, url } = options
-    const name = interaction.customId.split(':').at(-1) as string
-    const pastInteractionId = interaction.customId.split(':').at(-3) as string
-    color &&= colord(color).toHex()
+    const { color, description, footer, t, title, url } = options
+    const [id, lang] = JSON.parse(interaction.customId.split(':').at(-1) as string) as string[]
 
-    const images = temporaryImgStorage.get(pastInteractionId)
-    const newImages = images?.map((image) => ({ [image.type.toLowerCase()]: image.id }))
-      .reduce((previous, current) => ({ ...previous, ...current }))
+    if (lang) {
+      // todo: there is probably a better way to do this, I hate mongoose :((
+      const announcement = await Announcement.findById(id).exec()
+      if (!announcement) return
+      const translationIndex = announcement.translations.findIndex((translation) => translation.lang === lang)
+      const translation = announcement.translations[translationIndex]
 
-    const announcement = new Announcement({
-      color,
-      description,
-      footer,
-      guildId: interaction.guildId,
-      name,
-      title,
-      url,
-      ...newImages
-    })
-    await announcement.save()
-    temporaryImgStorage.delete(pastInteractionId)
+      // ? update translation object
+      announcement.translations[translationIndex] = {
+        ...translation,
+        description,
+        footer,
+        title,
+        url
+      }
+      await announcement.save()
 
-    return await reply(interaction, {
-      content: t('commands:add.done'),
-      type: 'positive'
-    })
+      return await reply(interaction, {
+        content: t('commands:edit.done_translation'),
+        type: 'positive'
+      })
+    } else {
+      const result = await Announcement.findOneAndUpdate(
+        { _id: id },
+        { color, description, footer, title, url }
+      ).exec().catch(() => {})
+
+      if (!result) return await reply(interaction, { content: t('commands.edit.defaultError'), type: 'negative' })
+      return await reply(interaction, {
+        content: t('commands:edit.done_announcement'),
+        type: 'positive'
+      })
+    }
   }
 }
