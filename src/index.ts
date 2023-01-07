fd791677406710e18b19c96199eee959982bfd72
Vicente <33096355+Vicente015@users.noreply.github.com>
2023-01-07 16:18:52 +0000

    ♻️ Small refactor

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import 'dotenv/config'
 import { LogLevel, SapphireClient } from '@sapphire/framework'
 import { InternationalizationContext } from '@sapphire/plugin-i18next'
 import { PresenceUpdateStatus } from 'discord-api-types/v10'
-import { Intents } from 'discord.js'
+import { Intents, Options } from 'discord.js'
 import { ActivityTypes } from 'discord.js/typings/enums'
 import config from '../config.json'
 
@@ -17,6 +17,22 @@ const client = new SapphireClient({
   },
   intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES],
   logger: { level: config.debug ? LogLevel.Debug : LogLevel.Info },
+  makeCache: Options.cacheWithLimits({
+    ...Options.defaultMakeCacheSettings,
+    GuildBanManager: 0,
+    GuildEmojiManager: 0,
+    GuildInviteManager: 0,
+    GuildScheduledEventManager: 0,
+    GuildStickerManager: 0,
+    MessageManager: 0,
+    PresenceManager: 0,
+    ReactionManager: 0,
+    ReactionUserManager: 0,
+    StageInstanceManager: 0,
+    ThreadManager: 0,
+    ThreadMemberManager: 0,
+    VoiceStateManager: 0
+  }),
   presence: {
     activities: [{ name: 'Publishing announcements', type: ActivityTypes.PLAYING }],
     status: PresenceUpdateStatus.Online
