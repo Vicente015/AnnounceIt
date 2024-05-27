eca470bf0b2b104031cf32a2f3ee4afe0bd6ce1a
Vicente <33096355+Vicente015@users.noreply.github.com>
2024-05-27 19:20:50 +0300

    tests: Add error tests

--- src/jobs/postScheduled.test.ts
+++ src/jobs/postScheduled.test.ts
@@ -2,7 +2,7 @@
 import { SapphireClient } from '@sapphire/framework'
 import dayjs from 'dayjs'
 import mongoose from 'mongoose'
-import { beforeEach, describe, expect, it } from 'vitest'
+import { beforeEach, describe, expect, it, vi } from 'vitest'
 import { Announcement } from '../schemas/Announcement.js'
 import { ScheduledPost } from '../schemas/ScheduledPost.js'
 import { mockTextChannel } from '../tests/channel-mock.js'
@@ -19,6 +19,80 @@ beforeEach(async () => {
 })
 
 describe('postScheduled', () => {
+  it('should not publish announcement if the date is later', async () => {
+    const guild = mockGuild(client)
+    const channel = mockTextChannel(client, guild)
+    const announcement = new Announcement({
+      description: 'fake announcement',
+      guildId: guild.id,
+      name: 'announcement1',
+      published: false,
+      translations: []
+    })
+    await announcement.save()
+
+    const oneHourLater = dayjs().utc().add(1, 'h')
+    const scheduledPost = new ScheduledPost({
+      announcementId: announcement.id as string,
+      at: oneHourLater.toISOString(),
+      channelId: channel.id
+    })
+    await scheduledPost.save()
+    await postScheduledJob(client)
+
+    const updatedAnnouncement = await Announcement.findById(announcement.id)
+    expect(updatedAnnouncement?.published).toBe(false)
+  })
+
+  it('should log error if the announcement is deleted', async () => {
+    const guild = mockGuild(client)
+    const channel = mockTextChannel(client, guild)
+    const announcement = new Announcement({
+      description: 'fake announcement',
+      guildId: guild.id,
+      name: 'announcement1',
+      published: false,
+      translations: []
+    })
+
+    const oneHourBefore = dayjs().utc().subtract(1, 'h')
+    const scheduledPost = new ScheduledPost({
+      announcementId: announcement.id as string,
+      at: oneHourBefore.toISOString(),
+      channelId: channel.id
+    })
+    await scheduledPost.save()
+
+    const loggerSpy = vi.spyOn(client.logger, 'error')
+    await postScheduledJob(client)
+    expect(loggerSpy).toHaveBeenCalled()
+  })
+
+  it('should log error if channel doesn\'t exist', async () => {
+    const guild = mockGuild(client)
+    const channel = mockTextChannel(client, guild)
+    const announcement = new Announcement({
+      description: 'fake announcement',
+      guildId: guild.id,
+      name: 'announcement1',
+      published: false,
+      translations: []
+    })
+
+    const oneHourBefore = dayjs().utc().subtract(1, 'h')
+    const scheduledPost = new ScheduledPost({
+      announcementId: announcement.id as string,
+      at: oneHourBefore.toISOString(),
+      channelId: channel.id
+    })
+    await scheduledPost.save()
+    client.channels.cache.delete(channel.id)
+
+    const loggerSpy = vi.spyOn(client.logger, 'error')
+    await postScheduledJob(client)
+    expect(loggerSpy).toHaveBeenCalled()
+  })
+
   it('should change the "published" property in the announcement document', async () => {
     const guild = mockGuild(client)
     const channel = mockTextChannel(client, guild)
@@ -38,7 +112,6 @@ describe('postScheduled', () => {
       channelId: channel.id
     })
     await scheduledPost.save()
-
     await postScheduledJob(client)
 
     const updatedAnnouncement = await Announcement.findById(announcement.id)
@@ -71,7 +144,7 @@ describe('postScheduled', () => {
     expect(lastMessage?.embeds[0].description).toBe(announcement.description)
   })
 
-  it('should delete the scheduled post', async () => {
+  it('should delete the scheduled post after posting', async () => {
     const guild = mockGuild(client)
     const channel = mockTextChannel(client, guild)
     const announcement = new Announcement({
